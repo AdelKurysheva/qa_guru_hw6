39e446f36e2c0d7ce44b519bcbe7b28cf192904a
Adel Kurysheva <Adel.Kurysheva@nedra.digital>
2023-03-19 17:28:38 +0300

    add JavaOperators

--- src/main/java/JavaOperators.java
+++ src/main/java/JavaOperators.java
@@ -6,22 +6,22 @@ public class JavaOperators {
         int z = -50;
         int t = -50;
         System.out.println("Простые операторы:");
-        System.out.println("x + y = "+(x+y));
-        System.out.println("x - z = "+(x-z));
-        System.out.println("xy = "+(x*y));
-        System.out.println("z/y = "+(z/y));
-        System.out.println("z % x = "+(z%x));
-        System.out.println("++x = "+(++x));
-        System.out.println("--y = "+(--y));
+        System.out.println("x + y = " + (x + y));
+        System.out.println("x - z = " + (x - z));
+        System.out.println("xy = " + (x * y));
+        System.out.println("z/y = " + (z / y));
+        System.out.println("z % x = " + (z % x));
+        System.out.println("++x = " + (++x));
+        System.out.println("--y = " + (--y));
 
         //логические операторы
         System.out.println("Логические операторы:");
-        System.out.println("x != y = "+ (x != y));
-        System.out.println("t == z = "+ (t == z));
-        System.out.println("x > y  = "+ (x > y));
-        System.out.println("t < z  = "+ (t < z));
-        System.out.println("t <= z = "+ (t <= z));
-        System.out.println("x >= y = "+ (x >= y));
+        System.out.println("x != y: " + (x != y));
+        System.out.println("t == z: " + (t == z));
+        System.out.println("x > y : " + (x > y));
+        System.out.println("t < z : " + (t < z));
+        System.out.println("t <= z: " + (t <= z));
+        System.out.println("x >= y: " + (x >= y));
 
         //переполнение
         System.out.println("Переполнение:");
@@ -33,12 +33,11 @@ public class JavaOperators {
         System.out.println("Комбинация типов данных:");
         double a = 4.5;
         int b = 3;
-        System.out.println("(a + b) = " + (a+b));
-        System.out.println("a - b = " + (a-b));
-        System.out.println("ab = " + a*b);
-        System.out.println("a/b = " + a/b);
-        System.out.println("a + b = " + a+b);
-
+        System.out.println("(a + b) = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("ab = " + a * b);
+        System.out.println("a/b = " + a / b);
+        System.out.println("a + b = " + a + b);
 
 
     }
